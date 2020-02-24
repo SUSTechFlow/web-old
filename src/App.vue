98d9021f3a43fc1749284b895a3beda326e233e0
john <799433746@qq.com>
2020-02-24 14:29:20 +0800

    Revert "随便改了一手"
    This reverts commit 5d1ad0b6
    
    Please open a new branch named wip, I still got a few other things to fix here, sorry.

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,37 @@
 <template>
   <div id="app">
-    <div class="layout">
-      <Layout>
-        <Header>
-          <Navbar></Navbar>
-        </Header>
-        <Content :style="{ margin: '0 50px', padding: '0 50px' }">
-          <Card>
-            <div style="min-height: 1200px;">
-              <router-view></router-view>
-            </div>
-          </Card>
-        </Content>
-        <Footer class="layout-footer-center">
-          <a style="top:auto; bottom: 0;" href="http://www.beian.miit.gov.cn"
-            >粤ICP备20009314号</a
-          ></Footer
-        >
-      </Layout>
+    <Navbar class="navbar"></Navbar>
+    <div class="content">
+      <router-view></router-view>
     </div>
+    <a
+      style="position:fixed; top:auto; bottom: 0;"
+      href="http://www.beian.miit.gov.cn"
+      >ç²¤ICPå¤‡20009314å�·</a
+    >
   </div>
 </template>
+
 <script>
 import Navbar from "./components/Navbar";
 
 export default {
   name: "app",
-  components: { Navbar }
+  components: { Navbar },
+  data() {
+    return {};
+  }
 };
 </script>
-<style scoped>
-.layout {
-  border: 1px solid #d7dde4;
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
+<style>
+.navbar {
+  top: 0;
+  position: fixed;
+  width: 100%;
 }
-.layout-footer-center {
-  text-align: center;
+.content {
+  width: 100%;
+  top: 64px;
+  position: absolute;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,109 +1,86 @@
 <template>
-  <Menu class="navbar" mode="horizontal" theme="dark" :active-name="activeItem">
-    <div class="layout-logo">SUSTech Flow</div>
-    <div class="layout-nav">
-      <MenuItem v-if="!this.loggedUser" to="/login" name="login">
-        <Icon type="ios-people" />
-        登录
-      </MenuItem>
-      <Submenu v-else name="user">
-        <template slot="title">
-          <Icon type="ios-people" />
-          {{ loggedUser }}
-        </template>
-        <MenuItem name="profile" to="/profile">
-          <Icon type="ios-person" />
-          个人页面
+    <Menu class="navbar" mode="horizontal" theme="light" :active-name="activeItem">
+        <MenuItem v-if="!this.loggedUser" to="/login" name="login">
+            <Icon type="ios-people"/>
+            登录
         </MenuItem>
-        <MenuItem name="logout">
-          <Icon type="ios-log-out" />
-          <Button
-            type="default"
-            to="/login"
-            id="logout"
-            name="logout"
-            @click="logout()"
-            >登出</Button
-          >
+        <Submenu v-else name="user">
+            <template slot="title">
+                <Icon type="ios-people"/>
+                {{ loggedUser }}
+            </template>
+            <MenuItem name="profile" to="/profile">
+                <Icon type="ios-person"/>
+                个人页面
+            </MenuItem>
+            <MenuItem name="logout">
+                <Icon type="ios-log-out"/>
+                <Button type="default" to="/login" id="logout" name="logout" @click="logout()">登出</Button>
+            </MenuItem>
+        </Submenu>
+        <MenuItem name="course" to="/course" >
+            <Icon type="ios-calendar" />
+            探索课程
         </MenuItem>
-      </Submenu>
-      <MenuItem name="course" to="/course">
-        <Icon type="ios-calendar" />
-        探索课程
-      </MenuItem>
-      <MenuItem name="guide" to="/guide">
-        <Icon type="md-build" />
-        使用指南
-      </MenuItem>
-      <MenuItem name="aboutme" to="/aboutme">
-        <Icon type="logo-android" />
-        关于开发者
-      </MenuItem>
-    </div>
-  </Menu>
+        <MenuItem name="guide" to="/guide" >
+            <Icon type="md-build" />
+            使用指南
+        </MenuItem>
+        <MenuItem name="aboutme" to="/aboutme" >
+            <Icon type="logo-android" />
+            关于开发者
+        </MenuItem>
+    </Menu>
+
 </template>
 
 <script>
-import { Menu, MenuItem } from "view-design";
-import util from "../libs/util";
-import { mapState } from "vuex";
+    import {Menu, MenuItem} from "view-design";
+    import util from '../libs/util'
+    import {mapState} from 'vuex'
 
-export default {
-  name: "Navbar",
-  data() {
-    return {
-      activeItem: "course"
-    };
-  },
-  components: {
-    Menu: Menu,
-    MenuItem: MenuItem
-  },
-  methods: {
-    logout() {
-      this.$store.commit("setLoggedUser", "");
-      localStorage.removeItem("temp_token");
-    },
-    async getLoggedUser() {
-      const res = await util.http.get("/user");
-      if (res.data.username)
-        this.$store.commit("setLoggedUser", res.data.username);
+    export default {
+        name: "Navbar",
+        data(){
+            return {
+                activeItem:"course"
+            }
+        },
+        components: {
+            'Menu': Menu,
+            'MenuItem': MenuItem
+        },
+        methods: {
+            logout() {
+                this.$store.commit('setLoggedUser', '');
+                localStorage.removeItem('temp_token');
+            },
+            async getLoggedUser() {
+                const res = await util.http.get('/user');
+                if (res.data.username)
+                    this.$store.commit('setLoggedUser', res.data.username)
+            }
+        },
+        created() {
+            this.getLoggedUser();
+        },
+        computed: mapState({
+            loggedUser: state => state.user.loggedUser
+        }),
+        watch:{
+            '$route' (to){
+                this.activeItem = to.name;
+                if (to.name === 'courseDetail')
+                    this.activeItem = 'course';
+            }
+        }
     }
-  },
-  created() {
-    this.getLoggedUser();
-  },
-  computed: mapState({
-    loggedUser: state => state.user.loggedUser
-  }),
-  watch: {
-    $route(to) {
-      this.activeItem = to.name;
-      if (to.name === "courseDetail") this.activeItem = "course";
-    }
-  }
-};
 </script>
 
 <style scoped>
-#logout {
-  position: relative;
-  right: 0;
-  left: 0;
-}
-
-.layout-logo {
-  width: 100px;
-  height: 30px;
-  border-radius: 3px;
-  float: left;
-  position: relative;
-  top: 15px;
-  left: 20px;
-}
-.layout-nav {
-  width: 420px;
-  margin: 0 auto;
-  margin-right: 20px;
-}
+    #logout {
+        position: relative;
+        right: 0;
+        left: 0;
+    }
 </style>
