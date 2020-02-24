e3668a635ae3a0752ccf42bd79afa50e37d2ff87
Wycer <wycers@gmail.com>
2020-02-24 12:32:48 +0800

    fix encoding

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,37 @@
 <template>
-    <div id="app">
-        <Navbar class="navbar"></Navbar>
-        <div class="content">
-            <router-view></router-view>
-        </div>
-        <a style="position:fixed; top:auto; bottom: 0;" href="http://www.beian.miit.gov.cn">粤ICP备20009314号</a>
+  <div id="app">
+    <Navbar class="navbar"></Navbar>
+    <div class="content">
+      <router-view></router-view>
     </div>
+    <a
+      style="position:fixed; top:auto; bottom: 0;"
+      href="http://www.beian.miit.gov.cn"
+      >ç²¤ICPå¤‡20009314å�·</a
+    >
+  </div>
 </template>
 
 <script>
-    import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 
-    export default {
-        name: 'app',
-        components: {Navbar},
-        data() {
-            return {}
-        }
-    }
+export default {
+  name: "app",
+  components: { Navbar },
+  data() {
+    return {};
+  }
+};
 </script>
 <style>
-    .navbar {
-        top: 0;
-        position: fixed;
-        width: 100%;
-    }
-    .content {
-        width: 100%;
-        top: 64px;
-        position: absolute;
-    }
+.navbar {
+  top: 0;
+  position: fixed;
+  width: 100%;
+}
+.content {
+  width: 100%;
+  top: 64px;
+  position: absolute;
+}
 </style>
